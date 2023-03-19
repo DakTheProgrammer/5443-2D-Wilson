bf3a7018863da1189205aa0bbd15c471250131a5
Dakota Wilson <dtw3200@live.com>
2023-03-19 14:03:56 -0500

    Multiplayer start working

---
+++ Assignments/P03/Messenger.py
@@ -0,0 +1,36 @@
+from Comms import CommsListener, CommsSender
+import sys
+import json
+
+class Messenger:
+    def __init__(self, creds, callback=None):
+        self.creds = creds
+        self.callBack = callback
+
+        if not self.creds:
+            print(
+                "Error: Message handler needs `creds` or credentials to log into rabbitmq. "
+            )
+            sys.exit()
+
+        if self.callBack != None:
+            # Start the comms listener to listen for incoming messages
+            self.commsListener.threadedListen(self.callBack)
+
+        self.user = self.creds["user"]
+
+        # create instances of a comms listener and sender
+        # to handle message passing.
+        self.commsListener = CommsListener(**self.creds)
+        self.commsSender = CommsSender(**self.creds)
+
+    def send(self, **kwargs):
+        """ """
+        target = kwargs.get("target", "broadcast")
+        self.commsSender.threadedSend(
+            target=target, sender=self.user, body=json.dumps(kwargs), debug=False
+        )
+
+    def setCallback(self, callBack):
+        self.callBack = callBack
+        self.commsListener.threadedListen(self.callBack)
--- Assignments/P03/main.py
+++ Assignments/P03/main.py
@@ -1,5 +1,28 @@
+import sys
 from GameDriver import GameDriver
+from Messenger import Messenger
 
-game = GameDriver('Game')
+#multiplayer needs: exchange(like a holder of the game), user, password = username + 2023!!!!!, port = 5672, host = terrywgriffin.com
+if len(sys.argv) > 1:
+    
+    try:
+        creds = {
+            'exchange': sys.argv[1],
+            'user': sys.argv[2],
+            'password': sys.argv[2] + '2023!!!!!',
+            'port': 5672,
+            'host': 'terrywgriffin.com'
+        }
+    except:
+        print('\n\nIncorrect arguments for multiplayer!!!')
+        print('\n\nShould look like: `python main.py exchange username`')
+        sys.exit()
+
+    Messenger(creds)
+
+    game = GameDriver('Game')
+    print('Multi')
+else:
+    game = GameDriver('Game')
 
 game.GameLoop()
