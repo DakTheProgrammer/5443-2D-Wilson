1094605c7e67b639077481dfc0e542e96790a980
Dakota Wilson <dtw3200@live.com>
2023-05-01 19:13:55 -0500

    scale of health bar

--- Assignments/P04/HealthBar.py
+++ Assignments/P04/HealthBar.py
@@ -4,10 +4,15 @@ from Tile import Tile
 class HealthBar:
     def __init__(self, sheet):
         self.tiles = []
+
+        self.scale = (32,32)
+
         for i in range(5):
-            self.tiles.append(Tile(sheet[531],pygame.rect.Rect(i*16,0,16,16),531))
+            self.tiles.append(Tile(sheet[531],pygame.rect.Rect(i*self.scale[0],0,self.scale[0],self.scale[0]),531))
+        
+            self.tiles[i].image = pygame.transform.scale(self.tiles[i].image, self.scale)
             
     def draw(self, screen, left, top):
         for i, tile in enumerate(self.tiles):
-            tile.rect.topleft = (left + (i * 16), top)
+            tile.rect.topleft = (left + (i * self.scale[0]), top)
             screen.blit(tile.image, tile.rect)
