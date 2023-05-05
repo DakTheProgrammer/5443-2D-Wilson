d462e069a5c5c2a5799df05abc6d4eed363bcff8
Leslie-N-Cook <leslienichole.co@gmail.com>
2023-05-05 10:14:21 -0500

    so close

--- Assignments/P04/LevelThree.py
+++ Assignments/P04/LevelThree.py
@@ -6,13 +6,18 @@ class LevelThree:
         self.__portalButtonTop = 51
         self.__portalButtonBottom = 300
         self.__portalLocation = portalLoc.rect.topleft
-        self.__leverTop = 303
+        self.__leverTopR = 303
+        self.__leverTopL = 215
+        self.__leverBottomR = 841
+        self.__leverBottomL = 753
         self.__leverTopCur = 'L'
         self.__leverBottomCur = 'L'  
-        self.__doorTop = [280,281,258,259]
-        self.__doorBottom = [809,810,831,832]
+        self.__doorTopL = [191,192,172,173]
+        self.__doorTopR = [280,281,258,259]
+        self.__doorBottomL = [818,819,796,797]
+        self.__doorBottomR = [729,730,710,711]
         
-        self.__topObjs = 450
+        self.__topObjs = 424
     
         self.__sheet = sheet
         
@@ -25,32 +30,59 @@ class LevelThree:
         return self.__portalLocation, 'P'
         
     def leverEvent(self, tiles, objNum):
-        if objNum == self.__leverTop:
+        
+        if objNum == self.__leverTopR:
             if self.__leverTopCur == 'L':
                 self.__leverTopCur = 'R'
                 tiles[objNum].updateState(self.__sheet, 1)
                 
-                for part in self.__doorTop:
+                for part in self.__doorTopR:
                     tiles[part].updateState(self.__sheet, 3)
+                    
+        if objNum == self.__leverTopL:
+            if self.__leverTopCur == 'L':
+                self.__leverTopCur = 'R'
+                tiles[objNum].updateState(self.__sheet, 1)
+                    
+                for part in self.__doorTopL:
+                    tiles[part].updateState(self.__sheet, 3)
+                    
             else:
                 self.__leverTopCur = 'L'
                 tiles[objNum].updateState(self.__sheet, -1)
                 
-                for part in self.__doorTop:
+                for part in self.__doorTopL:
                     tiles[part].updateState(self.__sheet, -3)
-        else:
+                
+                for part in self.__doorTopR:
+                    tiles[part].updateState(self.__sheet, -3)
+                    
+        if objNum == self.__leverBottomR:
             if self.__leverBottomCur == 'L':
                 self.__leverBottomCur = 'R'
                 tiles[objNum].updateState(self.__sheet, 1)
                 
-                for part in self.__doorBottom:
+                for part in self.__doorBottomR:
                     tiles[part].updateState(self.__sheet, 3)
+                    
+            if objNum == self.__leverBottomL:
+                if self.__leverBottomCur == 'L':
+                    self.__leverBottomCur = 'R'
+                    tiles[objNum].updateState(self.__sheet, 1)
+                
+                    for part in self.__doorBottomL:
+                        tiles[part].updateState(self.__sheet, 3)
+                        
             else:
                 self.__leverBottomCur = 'L'
                 tiles[objNum].updateState(self.__sheet, -1)
                 
-                for part in self.__doorBottom:
+                for part in self.__doorBottomL:
                     tiles[part].updateState(self.__sheet, -3)
+                for part in self.__doorBottomR:
+                    tiles[part].updateState(self.__sheet, -3)
+                    
+        
 
     def getTopObjs(self):
         return self.__topObjs
